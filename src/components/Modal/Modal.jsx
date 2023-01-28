d433e61a3e1db7ea1767868f492a4b1f7b5a35d1
Olena Bondar <olena.bondar1060@gmail.com>
2023-01-28 13:42:48 +0200

    uodate Module

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import css from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleClickByEscape);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleClickByEscape);
+  }
+
+  handleClickByEscape = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleClickByOverlay = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={css.overlay} onClick={this.handleClickByOverlay}>
+        <div className={css.modal}>
+          {/* {this.props.children} */}
+          <img alt="" src={this.props.largeImageURL} className={css.image} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
+
+Modal.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+};
